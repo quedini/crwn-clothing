a4962e875e4788e0da0b806a09483c827a60e1de
Joel Lester <jflester@gmail.com>
2020-04-06 01:03:01 -0500

    update menu-items to navigate withRouter Higher Order Component (HOC)

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import './homepage.styles.scss';
+import "./homepage.styles.scss";
 
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
-  <Directory />
+  <div className="homepage">
+    <Directory />
+  </div>
 );
 
 export default HomePage;
