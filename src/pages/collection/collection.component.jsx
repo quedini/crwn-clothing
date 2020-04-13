990d74bb4f4777e841b05f0b1a7c8eb0fc8ddec9
Joel Lester <jflester@gmail.com>
2020-04-12 21:57:25 -0500

    added advanced routes to the shop page using colleciton parameter. Created the corresponding colleciton page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import {
+  sellectCollection,
+  selectCollection,
+} from "../../redux/shop/shop.selectors";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className="collection">
+      <h2>{collection.title.toUpperCase()} Page </h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
