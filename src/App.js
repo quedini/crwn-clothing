0e3dc52129c85f310c0658f5cdd47809a3873297
Joel Lester <jflester@gmail.com>
2020-04-19 19:28:47 -0500

    File cleanup of orphanded components and functions

--- src/App.js
+++ src/App.js
@@ -10,20 +10,15 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {
-  auth,
-  createUserProfileDocument,
-  addCollectionAndDocuments,
-} from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
-import { selectionCollectionPreivew } from "./redux/shop/shop.selectors";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -37,7 +32,6 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
-            
     });
   }
 
@@ -72,7 +66,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArray: selectionCollectionPreivew,
 });
 
 const mapDispatchToProps = (dispatch) => ({
