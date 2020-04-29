2060de64e63a894b2369d65f13a4c0432b56bd06
Joel Lester <jflester@gmail.com>
2020-04-28 22:26:29 -0500

    updated to use redux thunk to perform async functions in reducer instead of components for Shop and Collection landing page

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,21 +1,34 @@
 import ShopActionTypes from "./shop.types";
 
-
-
 const INITIAL_STATE = {
-   collections: null
-}
+  collections: null,
+  isFetching: false,
+  errorMessage: undefined,
+};
 
 export const shopReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-      case ShopActionTypes.UPDATE_COLLECTIONS: 
+  switch (action.type) {
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
-          ...state,
-          collections:action.payload
-      }
-      default:
-        return state;
-    }
-  };
+        ...state,
+        isFetching: true,
+        collections: action.payload,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        collections: action.payload,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-  export default shopReducer;
+export default shopReducer;
